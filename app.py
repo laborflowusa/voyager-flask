b117ab487d12ed973fd2b58486dc6687076c80a6
laborflowusa <laborflowusa@gmail.com>
2026-05-21 15:15:06 -0400

    Fix formatting in app.py

--- app.py
+++ app.py
@@ -131,4 +131,4 @@ def robots():
     return app.response_class('User-agent: *\nAllow: /\nSitemap: https://voyager-flask.onrender.com/sitemap.xml\n', mimetype='text/plain')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True) 
